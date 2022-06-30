401e692ab117930fee9da541d3d223924c3047bb
refractionpcsx2 <refraction@gmail.com>
2022-06-30 21:15:52 +0100

    GS: Add Pre-Round Sprite hack
    This attempts to preproduce hardware behaviour, but falls down in a bunch of cases, hence the hack.

--- pcsx2/GameDatabase.cpp
+++ pcsx2/GameDatabase.cpp
@@ -367,6 +367,7 @@ static const char* s_gs_hw_fix_names[] = {
 	"estimateTextureRegion",
 	"PCRTCOffsets",
 	"PCRTCOverscan",
+	"preRoundSprites",
 	"mipmap",
 	"trilinearFiltering",
 	"skipDrawStart",
@@ -413,6 +414,7 @@ bool GameDatabaseSchema::isUserHackHWFix(GSHWFixId id)
 		case GSHWFixId::Deinterlace:
 		case GSHWFixId::Mipmap:
 		case GSHWFixId::TexturePreloading:
+		case GSHWFixId::PreRoundSprites:
 		case GSHWFixId::TrilinearFiltering:
 		case GSHWFixId::MinimumBlendingLevel:
 		case GSHWFixId::MaximumBlendingLevel:
@@ -750,6 +752,10 @@ void GameDatabaseSchema::GameEntry::applyGSHardwareFixes(Pcsx2Config::GSOptions&
 
 			case GSHWFixId::PCRTCOverscan:
 				config.PCRTCOverscan = (value > 0);
+				break;
+
+			case GSHWFixId::PreRoundSprites:
+				config.PreRoundSprites = (value > 0);
 				break;
 
 			case GSHWFixId::Mipmap:
